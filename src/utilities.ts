81d7b8e29d20db623b6bf579d6daf0ca644b36b2
Simen Owesen-Lein <W101150@internal.financecorp.biz>
2019-05-15 16:00:02 +0200

    Allow multiple words in search

--- src/utilities.ts
+++ src/utilities.ts
@@ -2,15 +2,19 @@ import { Recipe } from "./dataTypes/dataTypes";
 
 export function searchRecipes(recipes: Recipe[], recipeSearchValue: string): Recipe[] {
     return recipes.filter(recipe => {
-        const searchBasis =
+        const searchBasis = (
             recipe.title +
             recipe.instructions +
-            recipe.ingredients.map(ingredient => ingredient.title).join(" ");
+            recipe.ingredients.map(ingredient => ingredient.title).join(" ")
+        ).toLowerCase();
 
-        return searchBasis.toLowerCase().includes(recipeSearchValue.toLowerCase());
+        return recipeSearchValue
+            .toLowerCase()
+            .split(" ")
+            .filter(a => a)
+            .every(searchTerm => searchBasis.includes(searchTerm));
     });
 }
-
 
 export function getRandomId() {
     return Date.now() + "_" + Math.random();
